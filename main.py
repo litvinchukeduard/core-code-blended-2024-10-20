fbbfb4d46023c6a211053b2baae7416f22061bbf
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2024-10-20 19:01:27 +0300

    Colorama example

---
+++ main.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Back, Style
+# print(Fore.BLUE + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.BRIGHT + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+# print(Fore.MAGENTA + Back.CYAN + "magenta text with cyan backgroud" + Style.RESET_ALL)
+# print("Hello!")
+
+value = int(input(Fore.MAGENTA + "Enter number:" + Fore.YELLOW))
+print(Fore.GREEN + str(value))
